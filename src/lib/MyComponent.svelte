6f1799230680159c08a3a3016012759633260bb1
potatotomatobanana <116115216+potatotomatobanana@users.noreply.github.com>
2024-02-17 03:23:42 +0000

    .

--- src/lib/MyComponent.svelte
+++ src/lib/MyComponent.svelte
@@ -1,12 +1,35 @@
 <script>
     import { navigating } from "$app/stores"
     import MyInnerComponent from './MyInnerComponent.svelte'
+    import { onDestroy, onMount } from "svelte"
 
     navigating.subscribe(navigatingValue => {
-        console.log('navigating changed: ', navigating)
+        console.log('navigating changed: ', navigatingValue)
+    })
+
+    export let delay = 250
+
+    let timeoutId
+    let delayedPreloading = false
+
+    onMount(() => {
+        navigating.subscribe(navigatingValue => {
+            clearTimeout(timeoutId)
+            timeoutId = setTimeout(() => {
+                delayedPreloading = !!navigatingValue
+            }, delay)
+        })
+    })
+
+    onDestroy(() => {
+        clearTimeout(timeoutId)
     })
 </script>
 
 <p>Hello from MyComponent.svelte</p>
+
+{#if delayedPreloading}
+aaa
+{/if}
 
 <MyInnerComponent />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,8 @@
 <script>
-    import { Toaster } from "svelte-french-toast"
+    // import { Toaster } from "svelte-french-toast"
     // import { MyComponent } from '$test-76543';
-    // import { MyComponent } from 'test-76543';
+    import { MyComponent } from 'test-76543';
 </script>
 
-<Toaster />
-<!-- <MyComponent /> -->
+<!-- <Toaster /> -->
+<MyComponent />
