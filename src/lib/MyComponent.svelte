6e0a7f140cd8db5e12bde5469451c8dec53d8eaf
potatotomatobanana <116115216+potatotomatobanana@users.noreply.github.com>
2024-02-17 03:10:28 +0000

    .

--- src/lib/MyComponent.svelte
+++ src/lib/MyComponent.svelte
@@ -1,5 +1,10 @@
 <script>
+    import { navigating } from "$app/stores"
     import MyInnerComponent from './MyInnerComponent.svelte'
+
+    navigating.subscribe(navigatingValue => {
+        console.log('navigating changed: ', navigating)
+    })
 </script>
 
 <p>Hello from MyComponent.svelte</p>
