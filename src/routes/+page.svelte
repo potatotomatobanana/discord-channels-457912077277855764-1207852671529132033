29e9369d4a821b430d527676cf11822ac3f2771f
potatotomatobanana <116115216+potatotomatobanana@users.noreply.github.com>
2024-02-17 03:52:51 +0000

    .

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,10 @@
 <script>
     // import { Toaster } from "svelte-french-toast"
     // import { MyComponent } from '$test-76543';
-    import { MyComponent } from 'test-76543';
+    // import { MyComponent } from 'test-76543';
+    import { Toaster } from "test-svelte-french-toast-56765"
 </script>
 
-<!-- <Toaster /> -->
-<MyComponent />
+<Toaster />
+<!-- <MyComponent /> -->
+asdasdasd
