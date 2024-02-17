0f385877f5dd34e8303cc829fc5b201da3bd6266
potatotomatobanana <116115216+potatotomatobanana@users.noreply.github.com>
2024-02-17 03:05:26 +0000

    .

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script>
-    // import { MyComponent } from '$test-76543';
-    import { MyComponent } from 'test-76543';
+    import { MyComponent } from '$test-76543';
+    // import { MyComponent } from 'test-76543';
 </script>
 
 <MyComponent />
